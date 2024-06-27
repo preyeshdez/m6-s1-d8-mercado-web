32586d02e2f4fea5fcde7e2b0e127689d284010f
preyeshdez <p.reyeshdez@gmail.com>
2024-06-26 20:12:15 -0400

    Se crea la estructura de la pagina utilizando handlebars, se crea servidor utilizando express

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { create } from 'express-handlebars';
+import productos from './productos.js';
+
+import * as path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.use('/bootstrap', express.static(path.join(__dirname, "/node_modules/bootstrap/dist")));
+app.use('/jquery', express.static(path.join(__dirname, "/node_modules/jquery/dist")));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor en http://localhost:${PORT}`)
+})
+
+const hbs = create({
+	partialsDir: [
+		path.join(__dirname, "/views/partials/"),
+	],
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "./views"));
+
+app.get("/", (req, res) => {
+	res.render("dashboard", {
+		productos
+	})
+})
---
+++ productos.js
@@ -0,0 +1,34 @@
+let productos = [
+    {
+        id: 1,
+        nombre: "Banana",
+        imagen: "/assets/img/banana.png"
+    },
+    {
+        id: 2,
+        nombre: "Cebollas",
+        imagen: "/assets/img/cebollas.png"
+    },
+    {
+        id: 3,
+        nombre: "Lechuga",
+        imagen: "/assets/img/lechuga.png"
+    },
+    {
+        id: 4,
+        nombre: "Papas",
+        imagen: "/assets/img/papas.png"
+    },
+    {
+        id: 5,
+        nombre: "Pimentón",
+        imagen: "/assets/img/pimenton.png"
+    },
+    {
+        id: 6,
+        nombre: "Tomate",
+        imagen: "/assets/img/tomate.png"
+    }
+]
+
+export default productos;
